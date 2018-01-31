6815737fdfc1e6554a0233fb9a3b48a34b9e59f9
tshemsedinov <timur.shemsedinov@gmail.com>
2018-01-31 23:44:56 +0200

    Implement user registration and tabs

---
+++ server.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const fs = require('fs');
+const http = require('http');
+const Websocket = require('websocket').server;
+
+const index = fs.readFileSync('./index.html');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end(index);
+});
+
+server.listen(80, () => {
+  console.log('Listen port 80');
+});
+
+const ws = new Websocket({
+  httpServer: server,
+  autoAcceptConnections: false
+});
+
+const clients = [];
+
+ws.on('request', (req) => {
+  const connection = req.accept('', req.origin);
+  clients.push(connection);
+  console.log('Connected ' + connection.remoteAddress);
+  connection.on('message', (message) => {
+    const dataName = message.type + 'Data';
+    const data = message[dataName];
+    console.log('Received: ' + data);
+    clients.forEach((client) => {
+      if (connection !== client) {
+        client.send(data);
+      }
+    });
+  });
+  connection.on('close', (reasonCode, description) => {
+    console.log('Disconnected ' + connection.remoteAddress);
+    console.dir({ reasonCode, description });
+  });
+});
