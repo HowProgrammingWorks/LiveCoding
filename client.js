941e4be00f3cbc1b2dae16ac2bc48cc9e03bc5ff
tshemsedinov <timur.shemsedinov@gmail.com>
2018-02-01 19:41:46 +0200

    Implement source sync and switching

--- client.js
+++ client.js
@@ -3,15 +3,25 @@
 const socket = new WebSocket('ws://127.0.0.1/');
 
 const user = document.getElementById('user');
+const source = document.getElementById('source');
 const buttons = document.getElementById('buttons');
 const settings = document.getElementById('settings');
 const clients = {};
 let selectedClient = null;
 let selectedButton = null;
+let localUser = '';
 
 const toElement = (html) => new DOMParser()
   .parseFromString(html, 'text/html')
   .body.childNodes[0];
+
+const showSource = (clientName) => {
+  selectedClient = clientName;
+  const client = clients[clientName];
+  if (client) {
+    source.value = client.source;
+  }
+};
 
 const addClient = (client) => {
   const button = toElement(
@@ -21,19 +31,28 @@ const addClient = (client) => {
   button.clientName = client.name;
   button.addEventListener('click', () => {
     if (button.clientName !== selectedButton) {
-      if (selectedButton) {
-        selectedButton.className = '';
-      }
+      if (selectedButton) selectedButton.className = '';
       button.className = 'selected';
       selectedButton = button;
       selectedClient = button.clientName;
+      showSource(selectedClient);
     }
   });
-  clients[client.name] = { button, client, text: '' };
+  clients[client.name] = { button, client, source: '' };
+  return button;
+};
+
+const changeSource = (edit) => {
+  const client = clients[edit.name];
+  client.source = edit.value;
+  if (selectedClient === edit.name) {
+    showSource(edit.name);
+  }
 };
 
 user.addEventListener('keydown', (event) => {
   if (event.keyCode === 13) {
+    localUser = user.value;
     const event = {
       client: {
         name: user.value
@@ -41,18 +60,28 @@ user.addEventListener('keydown', (event) => {
     };
     settings.hidden = true;
     socket.send(JSON.stringify(event));
-    addClient(event.client);
+    const button = addClient(event.client);
+    button.className = 'selected';
   }
 });
 
-const keyup = (event) => {
+source.addEventListener('input', (event) => {
+  const client = clients[localUser];
+  client.source = source.value;
   socket.send(JSON.stringify({
-    cell: event.target.id,
-    value: event.target.value
+    edit: {
+      name: localUser,
+      value: source.value
+    }
   }));
-};
+});
 
 socket.onmessage = (event) => {
   const change = JSON.parse(event.data);
-  addClient(change.client);
+  console.log(event.data);
+  if (change.client) {
+    addClient(change.client);
+  } else if (change.edit) {
+    changeSource(change.edit);
+  }
 };
